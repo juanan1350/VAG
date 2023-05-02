4427840666ad41b13007773bd866e68cf2140c25
diego <juanan241@gmail.com>
2023-05-02 23:59:19 +0200

    tercero ni va

--- app/src/main/java/com/vag/vista/MainActivity.java
+++ app/src/main/java/com/vag/vista/MainActivity.java
@@ -1,17 +1,24 @@
 package com.vag.vista;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import android.os.Bundle;
 
 import com.vag.R;
+import com.vag.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private NavController mNavC;
+    private AppBarConfiguration mAppBarC;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavHostFragment navH = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
     }
 }
